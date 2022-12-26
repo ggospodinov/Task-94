be14b4ce22a7b62d60d83ab69eafcc500723edbb
ggospodinov <baigrozdan99@abv.bg>
2022-12-26 15:14:19 +0200

    all

---
+++ src/js/Application.js
@@ -0,0 +1,21 @@
+import EventEmitter from "eventemitter3";
+
+export default class Application extends EventEmitter {
+  static get events() {
+    return {
+      READY: "ready",
+    };
+  }
+
+  constructor() {
+    super();
+    this.emojis = [];
+    this.banana = "🍌";
+    this.emit(Application.events.READY);
+  }
+  setEmojis(emojis) {
+    this.emojis = emojis;
+  }
+
+  addBananas() {}
+}
